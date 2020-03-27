e8275272d026e7d1dc491325fc65987dabe31f08
Daniel Robbins <drobbins@funtoo.org>
2020-03-26 23:51:34 -0600

    updates
    merged:
    
      flora: 5f12509fab984a6aa75ace3639a6aa9c2d3e0fb9
      faustoo: 8995a28fea3ba9e7455655dcc667609198361d5f
      fusion809: 4c337539e56f6b05435e2f5d378107c4b430defe
      deadbeef: 53e0a41db6e7193bd83ef117f8055457f0990cb7
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: b5c5fb372baf7e11c938705dbe21cf9278f1efea

--- mail-client/thunderbird-bin/autogen.py
+++ mail-client/thunderbird-bin/autogen.py
@@ -11,12 +11,12 @@ def get_artifact(hub, version, arch):
 		moz_arch = "i686"
 	url = f"https://archive.mozilla.org/pub/thunderbird/releases/{version}/linux-{moz_arch}/en-US/thunderbird-{version}.tar.bz2"
 	final_name = f'thunderbird-bin_{moz_arch}-{version}.tar.bz2'
-	return hub.pkgtools.ebuild.Artifact(
+	return hub.pkgtools.ebuild.Artifact(hub,
 		url=url,
 		final_name=final_name
 	)
 
-async def generate(hub):
+async def generate(hub, **pkginfo):
 
 	json_data = await hub.pkgtools.fetch.get_page("https://product-details.mozilla.org/1.0/thunderbird_versions.json")
 	json_dict = json.loads(json_data)
@@ -24,13 +24,12 @@ async def generate(hub):
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		hub,
-		name="thunderbird-bin",
-		cat="mail-client",
+		**pkginfo,
 		version=version,
 		artifacts=[
 			get_artifact(hub, version, "amd64"),
 			get_artifact(hub, version, "x86"),
-			hub.pkgtools.ebuild.Artifact(url=f"https://dev.gentoo.org/~juippis/distfiles/lightning-{lightning_version}.tar.xz")
+			hub.pkgtools.ebuild.Artifact(hub, url=f"https://dev.gentoo.org/~juippis/distfiles/lightning-{lightning_version}.tar.xz")
 		],
 		lightning_version=lightning_version
 	)
